de5b2557f717099259914cb985a3f1c866f25627
Filip_Plachy <filipplachycz@gmail.com>
2020-11-15 17:46:35 +0100

    login session

--- webface/routes.py
+++ webface/routes.py
@@ -1,51 +1,85 @@
 from . import app
-from flask import render_template, request, session
+from flask import render_template, request, session, redirect, url_for, g
+from os import urandom
 
-
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def index():
-    name = request.form.get("name")
-    if name:
-        session["name"]=name
-    else:
-        name = "Uživatel bez jména"
-    title = 'Index'
-    return render_template('base.html.j2', title=title, name=name)
-
+    if g.user:
+        title = 'Index'
+        return render_template('base.html.j2', title=title, user=session['user'])
+    return redirect(url_for('login'))
 
 @app.route('/info/')
 def info():
-    title = 'Info'
-    return render_template('info.html.j2', title=title)
+    if g.user:
+        title = 'Info'
+        return render_template('info.html.j2', title=title)
+    return redirect(url_for('login'))
 
 @app.route('/Květák')
 def kvetak():
-    title = 'Květák'
-    return render_template('kvetak.html.j2', title=title)
+    if g.user:
+        title = 'Květák'
+        return render_template('kvetak.html.j2', title=title)
+    return redirect(url_for('login'))
 
 @app.route('/Kapusta')
 def kapusta():
-    title = 'Kapusta'
-    return render_template('kapusta.html.j2', title=title)
+    if g.user:
+        title = 'Kapusta'
+        return render_template('kapusta.html.j2', title=title)
+    return redirect(url_for('login'))
 
 @app.route('/Banány')
 def banany():
-    title = 'Banány'
-    return render_template('banany.html.j2', title=title)
+    if g.user:
+        title = 'Banány'
+        return render_template('banany.html.j2', title=title)
+    return redirect(url_for('login'))
 
 @app.route('/Kalkulačka', methods=["GET", "POST"])
 def formulky():
-    label = request.form.get("text")
-    if label:
-        try:
-            result = eval(label)
-        except:
-            result = "Error :)"
-    else:
-        result = ""
-    title = 'Kalkulačka'
-    return render_template('formula.html.j2', title=title, result=result)
+    if g.user:
+        label = request.form.get("text")
+        if label:
+            try:
+                result = eval(label)
+            except:
+                result = "Error :)"
+        else:
+            result = ""
+        title = 'Kalkulačka'
+        return render_template('formula.html.j2', title=title, result=result)
+    return redirect(url_for('login'))
+
+
+app.secret_key = urandom(24)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    wrongpassword = ""
+    if request.method == "POST":
+        session.pop('user', None)
+
+        if request.form['password'] == 'password':
+            session['user'] = request.form['username']
+            return redirect(url_for('index'))
+        wrongpassword = "No zkus to znova 😉"
+
+    title = 'login'
+    return render_template('login.html.j2', title=title, wrongpassword=wrongpassword)
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user' in session:
+        g.user = session['user']
+
+@app.route('/')
+def dropsession():
+    session.pop('user', None)
+    return render_template('login.html.j2')
 
 
 @app.errorhandler(404)
